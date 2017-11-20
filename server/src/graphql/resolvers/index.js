61edfb5ba706b80804d60e8ae15d96b5b2e84ac6
king0120 <king0120@gmail.com>
2017-11-19 22:03:15 -0500

    added graphql date

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -1,8 +1,16 @@
+import GraphQLDate from 'graphql-date';
+
 import TweetResolver from './tweet-resolvers';
 
 export default {
+  Date: GraphQLDate,
   Query: {
     getTweet: TweetResolver.getTweet,
     getTweets: TweetResolver.getTweets
+  },
+  Mutation: {
+    createTweet: TweetResolver.createTweet,
+    updateTweet: TweetResolver.updateTweet,
+    deleteTweet: TweetResolver.deleteTweet
   }
 };
